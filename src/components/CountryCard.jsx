2d6b9c1fad1b9caeb1e866f471845828b0a8ee06
Joseph Justin Opoku Mensah <josephjustin.om@gmail.com>
2024-12-21 17:40:28 +0000

    Implements CountryCard component

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,14 +1,22 @@
+import { useNavigate, useParams } from "react-router-dom";
+
 function CountryCard({country}) {
     const {flags, name, population, region, capital} = country;
 
+    const navigate = useNavigate();
+
+    const handleClick = (country) => {
+        navigate(`/${country}`)    
+    }
+
     return (
-        <>
+        <div onClick={() => handleClick(name.common)}>
             <img src={flags.svg} alt={flags.alt} />
             <h2>{name.common}</h2>
             <h3>Population: <span>{population.toLocaleString()}</span></h3>
             <h3>Region: <span>{region}</span></h3>
             <h3>Capital: <span>{capital[0]}</span></h3>
-        </>
+        </div>
     )
 }
 
