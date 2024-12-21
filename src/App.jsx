0c35d49e6706808361f8025268dc492bb8afd277
Joseph Justin Opoku Mensah <josephjustin.om@gmail.com>
2024-12-21 16:21:35 +0000

    Implements routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,16 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Filter from './components/Filter';
-import axios from 'axios';
-import Countries from './components/Countries';
+import {Routes, Route} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CountryPage from "./pages/CountryPage";
+import Header from "./components/Header";
 
 function App() {
-  const [term, setTerm] = useState('');
-  const [region, setRegion] = useState('');
-  const [countries, setCountries] = useState([]);
-
-  useEffect(() => {
-    const fetchCountries = async () => {
-      const response = await axios.get(`https://restcountries.com/v3.1/all?fields=cca3,flags,name,population,region,subRegion,capital,tld,currencies,languages,borders`);
-      setCountries(response.data);
-    }
-
-    fetchCountries();
-  }, []);
-
-  const filteredCountries = countries.filter(country => 
-    (country.name.common.toLowerCase().includes(term.toLowerCase()) && (region === "" ? country : country.region === region))
-  );
-
-  const handleTermChange = (event) => {
-    setTerm(event.target.value);
-  }
-
-  const handleRegionChange = (event) => {
-    setRegion(event.target.value);
-  }
-
   return (
-    <>
-      <Filter 
-        term={term} 
-        onTermChange={handleTermChange} 
-        region={region}
-        onRegionChange={handleRegionChange}
-      />
-      <Countries countries={filteredCountries} />
-    </>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<HomePage />} />
+        <Route path=":id" element={<CountryPage />} />
+      </Route>
+    </Routes>
   )
 }
 
