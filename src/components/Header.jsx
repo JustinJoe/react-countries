c28411b0c592e3b2117ecd34588c72ce25f57549
Joseph Justin Opoku Mensah <josephjustin.om@gmail.com>
2025-02-09 22:26:44 +0000

    Adds styling and implements theme toggling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
+import { FaRegMoon } from "react-icons/fa";
 import { Outlet } from "react-router-dom";
 
 function Header() {
+    const toggleTheme = () => {
+        document.body.classList.toggle("dark");
+        
+        const header = document.querySelector(".heading")
+        header.classList.toggle("dark-card")
+
+        const cards = [...document.getElementsByClassName('card')]
+        cards.map(card => card.classList.toggle("dark-card"))
+
+        const inputs = [...document.getElementsByClassName("filter")]
+        inputs.map(input => input.classList.toggle("dark-card"))
+    }
+
     return (
         <>
-        <div>
-            <h1>Where in the world?</h1>
-            <p>Dark Mode</p>
+        <div className="heading">
+            <div className="header container row">
+                <h1>Where in the world?</h1>
+                <div className="row" onClick={toggleTheme}>
+                    <FaRegMoon
+                        className="dark-icon"
+                    />
+                    <p>Dark Mode</p>            
+                </div>
+            </div>
         </div>
         <Outlet />
         </>
