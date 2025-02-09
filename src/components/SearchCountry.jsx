bf232878d367d358ed75bbab0b515a92e6c7dcc8
Joseph Justin Opoku Mensah <josephjustin.om@gmail.com>
2025-02-09 22:27:34 +0000

    Installs react-icons and adds some styling

--- src/components/SearchCountry.jsx
+++ src/components/SearchCountry.jsx
@@ -1,11 +1,17 @@
+import { FaSearch } from "react-icons/fa";
+
 function SearchCountry({term, onChange}) {
     return (
-        <input 
-            type="search"
-            placeholder="Search for country"
-            value={term}
-            onChange={onChange}
-        />
+        <>
+            <FaSearch className="search-icon"/>
+            <input 
+                className="filter"
+                type="search"
+                placeholder="Search for a country..."
+                value={term}
+                onChange={onChange}
+            />
+        </>
     )
 }
 
