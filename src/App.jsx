01d847c6d9632c3012acf566bf66fa5bac5e2d0a
Joseph Justin Opoku Mensah <josephjustin.om@gmail.com>
2025-02-09 22:17:05 +0000

    Implements routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import {Routes, Route} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CountryPage from "./pages/CountryPage";
 import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Header />}>
         <Route index element={<HomePage />} />
-        <Route path=":id" element={<CountryPage />} />
+        <Route path="/:id" element={<CountryPage />} />
       </Route>
     </Routes>
   )
